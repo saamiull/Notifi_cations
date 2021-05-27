0d73f168bee945f59988eac0a242d08ddc3fabd4
sami2905 <bsef18a028@pucit.edu.pk>
2021-05-27 23:11:26 +0500

    Display of Notification

--- Notifications/app/src/main/java/org/samiulhaq/notifications/MainActivity.java
+++ Notifications/app/src/main/java/org/samiulhaq/notifications/MainActivity.java
@@ -1,7 +1,12 @@
 package org.samiulhaq.notifications;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 
@@ -12,7 +17,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
+    @RequiresApi(api = Build.VERSION_CODES.O)
     public void ShowNotification(View view) {
+        // Sets an ID for the notification, so it can be updated.
+        int notifyID = 1;
+        String CHANNEL_ID = "my_channel_01";// The id of the channel.
+        CharSequence name = getString(R.string.channel_name);// The user-visible name of the channel.
+        int importance = NotificationManager.IMPORTANCE_HIGH;
+        NotificationChannel mChannel = new NotificationChannel(CHANNEL_ID, name, importance);
+        // Create a notification and set the notification channel.
+        Notification notification = new Notification.Builder(MainActivity.this)
+                .setContentTitle("New Message")
+                .setContentText("You've received new messages.")
+                .setSmallIcon(R.drawable.ic_baseline_notifications_active_24)
+                .setChannelId(CHANNEL_ID)
+                .build();
+
+        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+        notificationManager.createNotificationChannel(mChannel);
+
+        notificationManager.notify(1, notification);
     }
 }
